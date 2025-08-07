02c6d5874e53f8483741cb6d012b38e58d382859
vtiquet <valentin.tiquet@gmail.com>
2025-08-07 11:25:48 +0000

    Created files and fixed broken main.c's

--- 1-main.c
+++ 1-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include sort.h
+#include "sort.h"
+
 /**
  * create_listint - Creates a doubly linked list from an array of integers
  *
@@ -11,24 +12,27 @@
  */
 listint_t *create_listint(const int *array, size_t size)
 {
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
+
+	list = NULL;
+	while (size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+			return (NULL);
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
+	}
+	return (list);
 }
+
 /**
  * main - Entry point
  *
@@ -36,15 +40,17 @@ listint_t *create_listint(const int *array, size_t size)
  */
 int main(void)
 {
-    listint_t *list;
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-    list = create_listint(array, n);
-        return (1);
-    print_list(list);
-    printf(n);
-    insertion_sort_list(&list);
-    printf(n);
-    print_list(list);
-    return (0);
+	listint_t *list;
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	list = create_listint(array, n);
+	if (!list)
+		return (1);
+	print_list(list);
+	printf("\n");
+	insertion_sort_list(&list);
+	printf("\n");
+	print_list(list);
+	return (0);
 }
--- sort.h
+++ sort.h
@@ -2,7 +2,7 @@
 #define SORT_H
 
 #include <stdlib.h>
-#include <stdio.h>  
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,7 +20,6 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
